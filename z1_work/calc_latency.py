e59d9f9490f476f57a86deaebe99a56c78c8f1be
Instant Contiki <user@instant-contiki.(none)>
2016-11-18 20:39:27 +0100

    different apporach to reading from process that works better

--- z1_work/calc_latency.py
+++ z1_work/calc_latency.py
@@ -1,18 +1,27 @@
-#!/usr/bin/python
-import subprocess,sys
-## command to run - tcp only ##
-cmd = "./run_procs.sh"
- 
-## run it ##
-p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
- 
-## But do not wait till netstat finish, start displaying output immediately ##
+from subprocess import Popen, PIPE
+from time import sleep
+from fcntl import fcntl, F_GETFL, F_SETFL
+from os import O_NONBLOCK, read
+
+p = Popen(['python', 'shell.py'],
+        stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = False)# run the shell as a subprocess:
+# set the O_NONBLOCK flag of p.stdout file descriptor:
+flags = fcntl(p.stdout, F_GETFL) # get current p.stdout flags
+fcntl(p.stdout, F_SETFL, flags | O_NONBLOCK)
+# issue command:
+p.stdin.write('command')
+# let the shell output the result:
+sleep(0.1)
+# get the output
 while True:
-    out = p.stderr.read(1)
-    if out == '' and p.poll() != None:
+    try:
+        temp = read(p.stdout.fileno(), 1024)
+	temp.strip("()")
+	print type(temp)
+	if(temp == "Enter command: You entered: command\n"):
+		print "WE MADE IT"
+        print read(p.stdout.fileno(), 1024),
+    except OSError:
+        # the os throws an exception if there is no data
+        print '[No more data]'
         break
-    if out != '':
-	if(out == "from t1"):
-		sys.stdout.write("got t1")
-        sys.stdout.write(out)
-        sys.stdout.flush()
